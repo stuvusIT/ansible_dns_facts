9f5c0fd5da3f73de7e37df4e305912f9ca103e3b
Janne Heß <jannehess@gmail.com>
2018-07-24 19:16:45 +0000

    Fix types of pdns_auth_api_zones

--- scripts/facts.py
+++ scripts/facts.py
@@ -102,7 +102,7 @@ def removeStringFromObject(obj, string_to_search, replace_string):
 if __name__ == "__main__":
     myHostname = argv[1]
     hostvars = json.loads(open(argv[2]).read())
-    ret = []
+    ret = {}
     if 'pdns_auth_api_zones' in hostvars[myHostname]:
         ret = hostvars[myHostname]['pdns_auth_api_zones']
 
@@ -227,29 +227,25 @@ if __name__ == "__main__":
     # Remove DNS Templates
     if 'pdns_auth_api_zones' in hostvars[myHostname]:
         to_remove = []
-        for zone in ret:
-            if 'kind' in ret[zone] and ret[zone]['kind'] in ['Template']:
-                to_remove.append(zone)
+        for name,zone in ret.items():
+            if 'kind' in zone and zone['kind'] == 'Template':
+                to_remove.append(name)
         for entry in to_remove:
-            try:
-                del(ret[zone])
-            except KeyError:
-                continue
+            ret.pop(entry, None)
 
     # Secondaries
     if 'dns_facts_primary_servers' in hostvars[myHostname] and 'dns_facts_secondary_name' in hostvars[myHostname]:
         for hostname in hostvars[myHostname]['dns_facts_primary_servers']:
             if hostname not in hostvars or 'pdns_auth_api_zones' not in hostvars[hostname]:
                 continue
-            for zone in hostvars[hostname]['pdns_auth_api_zones']:
-                if zone['kind'] in ['Master', 'Native']:
-                    for ns in zone['records'][zone['name']]['NS']:
+            for name,contents in hostvars[hostname]['pdns_auth_api_zones'].items():
+                if 'kind' not in contents or contents['kind'] in ['Master', 'Native']:
+                    for ns in contents['records'][name]['NS']:
                         if 'c' in ns and ns['c'] == hostvars[myHostname]['dns_facts_secondary_name']:
-                            ret.append({
-                                'name': zone['name'],
+                            ret[name] = {
                                 'kind': 'Slave',
                                 'masters': [hostvars[hostname]['ansible_host']]
-                            })
+                            }
                             break
 
     print(json.dumps(ret))
